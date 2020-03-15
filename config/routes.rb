5157c5780f2eb7deec26933cbdd91f9f8eaa63f6
otakao <kazuhawk@gmail.com>
2020-03-15 20:05:36 +0900

    additional-aut-update_file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do  
     resources :messages, only: [:index, :create]
+  
     namespace :api do
-      resource :messages, only: :index, defaults: { format: 'json' }
+      resources :messages, only: :index, defaults: { format: 'json' }
     end
   end
 end
